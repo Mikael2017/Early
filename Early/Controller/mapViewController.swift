89b46c88be9325444b6ff1bacde0697c77edd4bb
annotations <benedictqa@gmail.com>
2019-01-20 23:33:12 +0200

    edited

--- Early/Controller/mapViewController.swift
+++ Early/Controller/mapViewController.swift
@@ -8,8 +8,6 @@
 
 import UIKit
 import MapKit
-
-
 class mapViewController: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
---
+++ Early/Controller/venus.swift
@@ -0,0 +1,25 @@
+//
+//  venus.swift
+//  Early
+//Created by annotations on 1/20/19.
+//  Copyright © 2019 Prom. All rights reserved.
+import MapKit
+import AddressBook
+
+class venus: NSObject, MKAnnotation
+{
+    let title : String?
+    let locationName: String?
+    let coordinate: CLLocationCoordinate2D
+    init(title : String, locationName : String?, coordinate:CLLocationCoordinate2D)
+    {
+        self.title = title
+        self.locationName = locationName
+        self.coordinate = coordinate
+        super.init()
+    }
+    
+}
+  
+
+
