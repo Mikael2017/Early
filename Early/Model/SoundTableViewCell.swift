76f1a7b8a92f39b617091dec497fb3d936aaee10
annotations <argawmichael@gmail.com>
2019-01-31 12:24:15 +0200

    Broken version

--- Early/Model/SoundTableViewCell.swift
+++ Early/Model/SoundTableViewCell.swift
@@ -2,13 +2,10 @@
 //  Early
 //  Created by annotations on 1/27/19.
 //  Copyright © 2019 Mikael Arage. All rights reserved.
-
-
 import UIKit
-import AVFoundation
+//import AVFoundation
 
 class SoundTableViewCell: UITableViewCell {
-     var audioPlayer = AVAudioPlayer()
     
     @IBOutlet weak var soundName: UILabel!
     @IBOutlet weak var soundImage: UIImageView!
@@ -16,12 +13,7 @@ class SoundTableViewCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-        do {
-            audioPlayer = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath :Bundle.main.path(forResource: "Ttera", ofType: "wav")!))
-        }
-        catch {
-            print(error)
-        }
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -30,17 +22,4 @@ class SoundTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
   
-    
-    @IBAction func playSound(_ sender: UIButton) {
-        audioPlayer.play()
-    }
-    
-    
-    @IBAction func Pause(_ sender: UIButton) {
-        
-    }
-    
-    @IBAction func Restart(_ sender: UIButton) {
-    }
-    
 }
